ca11d58a9517d79eb6950d216017173c1e6ce121
easp13 <easp13@gmail.comgit>
2018-10-30 21:02:19 +0200

    add form2

---
+++ DSCSPlayer/Form2.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace DSCSPlayer
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(92, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(524, 477);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 478);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Shown += new System.EventHandler(this.Form2_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form2_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ DSCSPlayer/Form2.cs
@@ -0,0 +1,54 @@
+﻿using DSCSLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace DSCSPlayer
+{
+    public partial class Form2 : Form
+    {
+        DSCS m_player;
+        int count = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public void SetPanel(DSCS player)
+        {
+            m_player = player;
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+        }
+
+        private void Form2_Shown(object sender, EventArgs e)
+        {
+            Rect r = new Rect(0, 0, panel1.Width, panel1.Height);
+            m_player.UpdateVideoWindow(r);
+            m_player.SetWindowHandle(panel1.Handle);
+        }
+
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+
+            Graphics g = panel1.CreateGraphics();
+            IntPtr hDC = g.GetHdc();
+            m_player.Repaint(hDC);
+            if (count == 10)
+                timer1.Enabled = false;
+        }
+    }
+}
